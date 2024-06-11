91516b4ed6bc188c9d01ae065ce7e34be7a2f81d
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-06-11 16:16:34 +0200

    Update options.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,6 +4,8 @@
 vim.keymap.set("n", "ss", ":split<cr>", { silent = true })
 vim.keymap.set("n", "vv", ":vsplit<cr>", { silent = true })
 vim.cmd('inoremap jj <Esc>')
+lvim.keys.insert_mode["<C-s>"] = "<cmd>lua vim.lsp.buf.signature_help()<cr>"
+
 
 -- --------------------------------------------------------
 -- LINE NUMBER
@@ -21,6 +23,10 @@ vim.opt.cursorcolumn = false
 -- COLORSCHEME
 -- --------------------------------------------------------
 lvim.colorscheme = "peachpuff"
+-- lvim.colorscheme = "retrobox"
+lvim.builtin.lualine.style = "default"
+lvim.builtin.lualine.options.theme = "dracula"
+
 
 -- --------------------------------------------------------
 -- LUALINE
@@ -34,6 +40,16 @@ lvim.builtin.lualine.options.theme = "molokai"
 -- --------------------------------------------------------
 lvim.plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "lunarvim/colorschemes" },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
 }
 
 -- --------------------------------------------------------
